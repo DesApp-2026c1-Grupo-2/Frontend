967ed619635407eeba9d382e373f222f58a554ff
Vanina Cejas Olson <111694856+VaninaCejasElizabethOlson@users.noreply.github.com>
2026-05-03 19:27:39 -0300

    Refactor routing to use Layout component

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,29 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../components/Layout";
+
 import Dashboard from "../pages/Dashboard";
-import Laboratorios from "../pages/laboratorios";
-import Pedidos from "../pages/pedidos";
-import LogIn from "../pages/logIn";
-import Equipamiento from "../pages/equipamiento";
 import LaboratoriosMB from "../pages/laboratoriosMB";
 import LaboratoriosHC from "../pages/laboratoriosHC";
 import LaboratoriosMA from "../pages/laboratoriosMA";
 import LaboratoriosSM from "../pages/laboratoriosSM";
+import Pedidos from "../pages/pedidos";
+import Equipamiento from "../pages/equipamiento";
+import LogIn from "../pages/logIn";
+import Inventario from "../pages/inventario";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<Dashboard />} />
-        <Route path="/laboratorios/mb" element={<LaboratoriosMB />} />
-        <Route path="/laboratorios/hc" element={<LaboratoriosHC />} />
-        <Route path="/laboratorios/ma" element={<LaboratoriosMA />} />
-        <Route path="/laboratorios/sm" element={<LaboratoriosSM />} />
-        <Route path="/pedidos" element={<Pedidos />} />
-        <Route path="/equipamiento" element={<Equipamiento />} />
+        {/* RUTAS CON NAVBAR */}
+        <Route element={<Layout />}>
+          <Route path="/*" element={<Dashboard />} />
+          <Route path="/laboratorios/mb" element={<LaboratoriosMB />} />
+          <Route path="/laboratorios/hc" element={<LaboratoriosHC />} />
+          <Route path="/laboratorios/ma" element={<LaboratoriosMA />} />
+          <Route path="/laboratorios/sm" element={<LaboratoriosSM />} />
+          <Route path="/inventario" element={<Inventario />} />
+          <Route path="/equipamiento" element={<Equipamiento />} />
+          <Route path="/pedidos" element={<Pedidos />} />
+        </Route>
+
+        {/* RUTAS SIN NAVBAR (ej: login) */}
         <Route path="/logIn" element={<LogIn />} />
       </Routes>
     </BrowserRouter>
-    
   );
 }
 
