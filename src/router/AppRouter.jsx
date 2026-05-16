ff2bea720e89475fb187333ffb682fa79a10661f
franiel <66392105+franiiel@users.noreply.github.com>
2026-05-15 22:02:31 -0300

    add AuthContext y rutas protegidas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout";
+import PrivateRoute from "../components/PrivateRoute";
+import { AuthProvider } from "../context/AuthContext";
 
 //import Dashboard from "../pages/Dashboard";
 //import LaboratoriosMB from "../pages/laboratoriosMB";
@@ -14,26 +16,30 @@ import Laboratorios from "../pages/laboratorios";
 
 function AppRouter() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* RUTAS CON NAVBAR */}
-        <Route element={<Layout />}>
-          {/*<Route path="/*" element={<Dashboard />} />
-          <Route path="/laboratorios/mb" element={<LaboratoriosMB />} />
-          <Route path="/laboratorios/hc" element={<LaboratoriosHC />} />
-          <Route path="/laboratorios/ma" element={<LaboratoriosMA />} />
-          <Route path="/laboratorios/sm" element={<LaboratoriosSM />} />//*/}
-          <Route path="/" element={<Edificios />} /> {/* primer vista del administrador?????? */}
-          <Route path="/edificios/:id/laboratorios" element={<Laboratorios />} /> {/* laboratorios de un edificio específico */}
-          <Route path="/edificios" element={<Edificios />} />
-          <Route path="/equipamiento" element={<Equipamiento />} />
-          <Route path="/pedidos" element={<Pedidos />} />
-        </Route>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* RUTAS SIN NAVBAR (ej: login) */}
+          <Route path="/logIn" element={<LogIn />} />
 
-        {/* RUTAS SIN NAVBAR (ej: login) */}
-        <Route path="/logIn" element={<LogIn />} />
-      </Routes>
-    </BrowserRouter>
+          {/* RUTAS PROTEGIDAS Y CON NAVBAR */}
+          <Route element={<PrivateRoute />}>
+            <Route element={<Layout />}>
+              {/*<Route path="/*" element={<Dashboard />} />
+              <Route path="/laboratorios/mb" element={<LaboratoriosMB />} />
+              <Route path="/laboratorios/hc" element={<LaboratoriosHC />} />
+              <Route path="/laboratorios/ma" element={<LaboratoriosMA />} />
+              <Route path="/laboratorios/sm" element={<LaboratoriosSM />} />//*/}
+              <Route path="/" element={<Edificios />} /> {/* primer vista del administrador?????? */}
+              <Route path="/edificios/:id/laboratorios" element={<Laboratorios />} /> {/* laboratorios de un edificio específico */}
+              <Route path="/edificios" element={<Edificios />} />
+              <Route path="/equipamiento" element={<Equipamiento />} />
+              <Route path="/pedidos" element={<Pedidos />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
