cece2b95ae1e62b5c341462e8ae70673677d5343
VaninaCejasElizabethOlson <vaninaelizabeth.cejasolson@estudiantes.unahur.edu.ar>
2026-05-24 18:51:01 -0300

    Primer diseño del Dashboard

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 import PrivateRoute from "../components/PrivateRoute";
 import { AuthProvider } from "../context/AuthContext";
 
-//import Dashboard from "../pages/Dashboard";
+import Dashboard from "../pages/Dashboard"; /*PRUEBA */
 import Pedidos from "../pages/pedidos";
 import Equipamiento from "../pages/equipamiento";
 import LogIn from "../pages/logIn";
@@ -21,11 +21,7 @@ function AppRouter() {
           {/* RUTAS PROTEGIDAS Y CON NAVBAR */}
           <Route element={<PrivateRoute />}>
             <Route element={<Layout />}>
-              {/*<Route path="/*" element={<Dashboard />} />
-              <Route path="/laboratorios/mb" element={<LaboratoriosMB />} />
-              <Route path="/laboratorios/hc" element={<LaboratoriosHC />} />
-              <Route path="/laboratorios/ma" element={<LaboratoriosMA />} />
-              <Route path="/laboratorios/sm" element={<LaboratoriosSM />} />//*/}
+              <Route path="/*" element={<Dashboard />} /> {/*PRUEBA: dashboard  */}
               <Route path="/" element={<Edificios />} /> {/* primer vista del administrador?????? */}
               <Route path="/edificios/:id/laboratorios" element={<Laboratorios />} /> {/* laboratorios de un edificio específico */}
               <Route path="/edificios" element={<Edificios />} />
