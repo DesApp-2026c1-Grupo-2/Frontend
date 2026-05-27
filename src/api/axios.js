19ef2961133f3cf1a020d8686c0c806f19e94298
leonel-pisani <leonel.pisani@estudiantes.unahur.edu.ar>
2026-05-27 18:06:15 -0300

    enviar el token automaticamente en Axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -9,4 +9,16 @@ const api = axios.create({
   },
 });
 
+// INTERCEPTOR
+api.interceptors.request.use((config) => {
+
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
