68c91af38379488f7aa4ed40714d9e4f55943eb1
ethneazarate <ethneazarate@gmail.com>
2026-05-28 16:59:34 -0300

    Nuevo diseño de pagina laboratorios + fondo todo gris

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className="min-h-screen bg-slate-50">
+    <div className="min-h-screen bg-slate-100">
       <Navbar />
 
       <div className="pt-[70px] px-4 sm:px-6 lg:px-8">
