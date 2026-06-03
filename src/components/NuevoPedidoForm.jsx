c38519d4b4ebf4617d11aeae069b6a84f37fbc8f
ethneazarate <ethneazarate@gmail.com>
2026-06-03 13:16:02 -0300

    Eliminar funcionando, editar no se

--- src/components/NuevoPedidoForm.jsx
+++ src/components/NuevoPedidoForm.jsx
@@ -5,7 +5,9 @@ import { FiX } from "react-icons/fi";
 
 const STEPS = ["Datos Básicos", "Recursos", "Resumen", "Enviado"];
 
-export default function NuevoPedidoForm({ onClose, onCrear }) {
+export default function NuevoPedidoForm({ onClose, onCrear, pedidoInicial = null,
+  modo = "crear" }) {
+
   const { user } = useAuth();
   const [step, setStep] = useState(0);
   const [loading, setLoading] = useState(true);
@@ -22,6 +24,36 @@ export default function NuevoPedidoForm({ onClose, onCrear }) {
     laboratorio: "", 
     recursos: [], 
   });
+
+  useEffect(() => {
+    if (!pedidoInicial) return;
+
+    const fecha = pedidoInicial.fechaHora
+      ? pedidoInicial.fechaHora.split("T")[0]
+      : "";
+
+    const hora = pedidoInicial.fechaHora
+      ? new Date(pedidoInicial.fechaHora)
+          .toISOString()
+          .slice(11, 16)
+      : "10:00";
+
+    setForm({
+      materia: pedidoInicial.materia || "",
+      docente: pedidoInicial.docente?._id || "",
+      alumnos: pedidoInicial.alumnos || "",
+      fecha,
+      hora,
+      laboratorio:
+        pedidoInicial.laboratorio?.id ||
+        pedidoInicial.laboratorio?._id ||
+        "",
+      recursos: [],
+    });
+
+    console.log("FORM CARGADO");
+
+  }, [pedidoInicial]);
 
   const alumnos = Number(form.alumnos || 0);
 
@@ -166,6 +198,9 @@ export default function NuevoPedidoForm({ onClose, onCrear }) {
   };
 
   const handleCrear = async () => {
+    console.log("RECURSOS FORM");
+    console.log(form.recursos);
+
     const payload = {
       materia: form.materia,
       docente: form.docente,
